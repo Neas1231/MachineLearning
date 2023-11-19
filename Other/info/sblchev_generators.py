29eefe5f15fea9aabdae5e1fe141c0bb7a4aa892
Neas1231 <120177610+Neas1231@users.noreply.github.com>
2023-11-19 21:41:24 +0300

    Create sblchev_generators.py

---
+++ Other/info/sblchev_generators.py
@@ -0,0 +1,253 @@
+# list = array
+
+# test = []
+# # Undefined
+
+# test1 = ''
+# test2 = None
+# test.append('a')
+# print(test)
+# ==========================
+
+# def test(num):
+#     num = num + 10
+#     return num
+
+# print(test(10))
+
+
+# test = None
+# print(type(test))
+
+
+# def test():
+#     return (111, 4455, 455), ('вдыао',)
+
+# var, var1 = test()
+
+# print(var)
+# print(var1)
+# print(test())
+
+# test = []
+# print(test)
+
+
+# test = []
+# for i in range(10):
+# #     test.append(i)
+# def check(num):
+#     if num % 2 == 0:
+#         return num*2
+#     return num
+
+# # test = [i for i in range(10)]
+
+# test = [1,2,7,66,55,444,5,68,8,999,9,[2,3,5,6,7], [45,67,78,9]]
+# [x for x in range(11)]
+# for elem in test:
+#     print(elem)
+# string_num = [str(elem) for elem in test]
+# print(string_num)
+# var = 9
+# val = 0
+# string = [val := var+val for _ in test]
+# res = []
+# val1 = None
+
+# string =[  res.extend(val) if type(val) == list else res.append(val)   for val in test      ]
+# print(res)
+# # def generat():
+#     for val in test:
+#         if type(val) == list:
+#             for val1 in val:
+                
+#                 return val
+
+# test = [1,2,7,66,55,444,5,68,8,999,9,[2,3,5,6,7], [45,67,78,9], 10 , 90]
+
+
+# test = [1,2,7,66,55,444,5,68,8,999,9,[2,3,5,6,7], [45,67,78,9], 10 , 90]
+# test[10:11] = ['Python', 'язык', 'программирования']
+
+# print(test)
+
+# print(test[11])
+# test.insert(11, )
+# print(test)
+# test1 = [20,[21,22],22]
+# test.extend(test1)
+# print(test)
+
+# res = []
+# for elem in test:
+#     if type(elem) == list:
+
+#         # for ele in elem:
+#         #     res.append(ele)
+#     else:
+#         res.append(elem)
+# print(res)
+    
+
+
+
+# test1 = True if (test==10 and test2 < 200) else False
+# test1 = 10
+# test2 = 20
+# test = (test1, test2) if test1
+
+# print(test1)
+# new_numbers = [n + 1 if n >= 45 else n + 5 for n in numbers]
+
+
+# test = [ 11, 2, 56, 'текст', '56', '#']
+
+      
+        
+
+# Лаба № N
+# Написать функцию, которая на входе получает последовательность,
+# на выходе получить последовательность состоящую только из цифр
+
+
+
+
+# for val in test:
+#     for word in val:
+#         string.append(val+word)
+
+# print(string)
+# string = []
+# for elem in test:
+#     string.append(check(elem))
+
+# print(string)
+
+
+# print(test)
+
+
+# test = list(i for i in range(10))
+# print(test)
+
+# test = 
+
+
+# def test():
+#     # print('запуск взятия файла')
+#     # file = open('K:\\big_file.txt', encoding='latin-1')
+#     # # input()
+#     # super_var = file.read(100)
+#     # print('запуск взятия файла')
+#     # value = 'это дефаулт'
+#     value = None
+#     for i in range(1,10):
+        
+#         # if i > 1:
+#         #     yield False
+#             # return True
+#         # yield i # <--
+#         # value = yield f'{value} {i}' if value else i
+#         yield i
+#         # yield f'{value} {i}'
+#         # print(value, i)
+        
+        
+#     # for elem in my_list:
+#     #     yield elem
+#         # return True
+# test1 = test()
+# test2 = next(test1)
+# print(test2)
+# # test2 = test1.send('НОВЫЙ параметр 1')
+# test2 = next(test1)
+# print(test2)
+# # test2 = test1.send('НОВЫЙ параметр 2')
+# print(test2)
+# # test2 = test1.send('НОВЫЙ параметр 3')
+# print(test2)
+# test2 = next(test1)
+# print(test2)
+# # test2 = next(test1)
+# print(test2)
+
+
+
+
+# while True:
+#     input()
+#     res = next(test1)
+#     if res:
+#         print(res)
+#     else:
+#         print('это был последний элемент')
+#         # input()
+#         test1.close()
+#         # input()
+#         # del test1
+#         break
+# print(test1)
+# print('завершение работы программы')
+
+
+
+
+# print(next(test1))
+
+# gener = test([4,5,6,7,11,8,9,0])
+
+
+
+# [G4]К сожалению, я не могу предоставить точный ответ на ваш вопрос, так как мне неизвестна конкретная ошибка в питании, о которой вы говорите. Однако, ошибка UnicodeDecodeError обычно возникает, когда пытаетесь декодировать байты в строку, используя неправильную кодировку. Возможно, вам потребуется проверить кодировку и убедиться, что она соответствует ожидаемому формату. Если проблема сохраняется, рекомендуется обратиться к документации или сообществу разработчиков для получения более конкретной помощи.
+
+
+# test1 = next(gener)
+# print(test1)
+# test1 = next(gener)
+# print(test1)
+# print(next(gener))
+# print(next(gener))
+
+# generate = ['первые элемент','это второй элемент','это третий элемент',5,6,7]
+# generate = iter(generate)
+
+
+# test1 = next(generate)
+# print(test1)
+
+
+# test = (num for num in range(1000000000000000000000000000000000))
+# print(test)
+# test1 = test.get()
+# print(test1)
+
+# test = (num for num in range(3))
+
+# for elem in test:
+#     print(elem)
+
+# print(generate)
+# while True:
+    
+#     try:
+#         print(next(generate))
+#         input()
+#     except:
+#         print('итератор закончился')
+#         break
+
+# # print(test)
+# print(next(test))
+# print(next(test))
+# print(next(test))
+# print(next(test))
+
+# Лаба № n
+# создайте текстовый файл
+# при запуске генератора определить по сколько байт за раз вы будете читать
+# после запуска генератора, он читает файл с заданным количеством байт с каждым итератором.
+
+
+
+
