f63eed69240a470b9a9a3609f07c7427f67e2c76
Neas1231 <120177610+Neas1231@users.noreply.github.com>
2024-01-12 02:30:45 +0300

    Updates

--- ML/PROducts/Neuro/test.py
+++ ML/PROducts/Neuro/test.py
@@ -4,4 +4,9 @@ from skimage.util import img_as_float
 from skimage.segmentation import slic
 from matplotlib import pyplot as plt
 import os
-image = img_as_float(io.imread(f'./datasets/landscapes/10}'))
+image = img_as_float(io.imread(f'./datasets/X/seg_landscapes/seg_0.jpg'))
+fig, ax = plt.subplots()
+ax.tick_params(axis='both', length=0)
+ax.set_xticklabels('')
+ax.set_yticklabels('')
+ax.imshow(image)
